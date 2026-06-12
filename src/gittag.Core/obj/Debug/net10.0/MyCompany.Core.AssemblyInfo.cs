efe3939173e3743ed5bab4546ee4a557c523f271
ad-youbrajsingh <youbraj.singh@aurionpro.com>
2026-06-12 11:45:05 +0530

    test commit 2

--- src/gittag.Core/obj/Debug/net10.0/MyCompany.Core.AssemblyInfo.cs
+++ src/gittag.Core/obj/Debug/net10.0/MyCompany.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCompany.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292db3b706187e61d521b8b0659a758c7364b782")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCompany.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCompany.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
